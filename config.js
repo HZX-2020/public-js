b9e0ef46cab4bd8c42e4219cc2a7c5900a6175c0
HZX <18004802730@163.com>
2022-08-28 00:36:32 +0800

    first

---
+++ config.js
@@ -0,0 +1,20 @@
+config = {
+  image: [
+    {
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile.moyublog.com%2Fd%2Ffile%2F2021-02-17%2Ff00a8b8358c97648073b8ed52e11b217.jpg&refer=http%3A%2F%2Ffile.moyublog.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=0404f342a772d8762db982f7fb16e346',
+      description:'图片1介绍'
+    },
+    {
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg1.duote.com%2Fduoteimg%2Fdtnew_newsup_img%2F202109%2F20210901111857_67190.jpeg&refer=http%3A%2F%2Fimg1.duote.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=59fe95714c5fb0fd93104528e32734f7',
+      description:'图片2介绍'
+    },
+    {
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fmobile%2F2020-07-21%2F5f16a799eabc3.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=64885da85b15ea9c588e3817408d271b',
+      description:'图片3介绍'
+    },
+    // {
+    //   url: '',
+    //   description:'图片4'
+    // },
+  ]
+}
