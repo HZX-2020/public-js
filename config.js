039e5354ad79944e8720477c0c60b72dc51baa82
HZX-2020 <1016261360@qq.com>
2022-08-28 00:56:52 +0800

    更新图片

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 config = {
   image: [
     {
-      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile.moyublog.com%2Fd%2Ffile%2F2021-02-17%2Ff00a8b8358c97648073b8ed52e11b217.jpg&refer=http%3A%2F%2Ffile.moyublog.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=0404f342a772d8762db982f7fb16e346',
+      url: 'https://pics5.baidu.com/feed/6a63f6246b600c338f232b988e9fbc08d8f9a167.png?token=71601b83b9040dd565d1e791f37f242b&s=C2609F5042417AECBE845965030070E0',
       description:'图片1介绍'
     },
     {
-      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg1.duote.com%2Fduoteimg%2Fdtnew_newsup_img%2F202109%2F20210901111857_67190.jpeg&refer=http%3A%2F%2Fimg1.duote.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=59fe95714c5fb0fd93104528e32734f7',
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fpic%2F1%2F09%2F89556dace6.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1664210898&t=b442804ee036c6def85058e2924b795a',
       description:'图片2介绍'
     },
     {
-      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fmobile%2F2020-07-21%2F5f16a799eabc3.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660318441&t=64885da85b15ea9c588e3817408d271b',
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F102920112646%2F201029112646-4-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1664210898&t=375d048a9fd19a55eb7b41c9945d3ea9',
       description:'图片3介绍'
     },
-    // {
-    //   url: '',
-    //   description:'图片4'
-    // },
+    {
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F062021132H5%2F210620132H5-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1664210898&t=a7a0687677d859294c3212a3cd9b7174',
+      description:'图片4介绍'
+    },
   ]
 }
